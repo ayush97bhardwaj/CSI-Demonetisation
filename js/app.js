13cfbcd54cf182d753b0cc7f766689890c7424c7
ayush97bhardwaj <ayush97bhardwaj@gmail.com>
2016-12-28 14:55:38 +0530

    added review form

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 (function(){
     var app=angular.module('demon', []);
 
+    app.controller('totalController', function(){
+        this.review=reviews;
+    });
+
     app.controller('SelectTabs', function(){
         this.tab=1;
         this.selected=false;
@@ -12,4 +16,21 @@
             return this.tab===value;
         };
     });
+
+    app.controller('ReviewController', function(){
+        this.review={};
+        this.addReview=function(product){
+                product.review.push(this.review);
+                this.review={};
+        };
+    });
+
+    reviews=[{
+        body: "I support Demonitisation. It is a very good step towards ending corruption",
+        author: "ayush@gmail.com",
+    },
+    {
+        body: "There are a lot of problems because of Demonitisation. The govt. should do something soon.",
+        author: "bhardwaj@yahoo.com",
+    }];
 })();
